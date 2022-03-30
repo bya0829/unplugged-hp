aed7aa4cc2fbb7e719e9f36cb625173e10ff5559
bya0829 <102687399+bya0829@users.noreply.github.com>
2022-03-31 02:09:10 +0900

    Update shinkan.vue

--- pages/shinkan.vue
+++ pages/shinkan.vue
@@ -6,8 +6,8 @@
       後期の新歓は行わない予定です。
     </p>
     
-    <h2>新歓ビラ2021</h2>
-    <img src="/uploads/2021_flyer_1.jpg" alt="" title="新歓ビラ2021" class="img-fluid" />
+    <h2>新歓ビラ2022</h2>
+    <img src="/uploads/2021_flyer_1.jpg" alt="" title="新歓ビラ2022" class="img-fluid" />
     <h2>ライブ動画</h2>
     <p>
       お待たせしました！<br>
